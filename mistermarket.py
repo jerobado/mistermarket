8137625c5916ecf855e2e646c79154cf0c964d19
Jero Bado <tokidokitalkyou@gmail.com>
2020-02-14 09:58:59 +0800

    Separeted scraping of equity and uitf urls
    _search_uitf - will search for UITF PH's website
    _search_equity - will search for Investegrams' website
    
    I'm still waiting for their consent if we are allowed to scrape their website for the latest price. Let's just for their 'expected' response.

--- mistermarket.py
+++ mistermarket.py
@@ -23,21 +23,48 @@ import requests
 
 class MrMarket:
 
-    def __init__(self, ticker):
+    def __init__(self, ticker, market=None):
 
-        self.company = str
-        self.price = float
         self.ticker = ticker
-        self._search_ticker(ticker)
+        self.market = market
+        self.company = None
+        self.price = None
+        self._search_ticker()
 
-    def _search_ticker(self, ticker):
+    def _search_ticker(self):
 
         try:
-            investegram_url = f'https://www.investagrams.com/stock/{ticker}'
-            response = requests.get(investegram_url)
-            soup = bs4.BeautifulSoup(response.content, 'html.parser')
-            self.company = soup.find('h4', class_='mb-0').find('small').get_text(strip=True)
-            self.price = float(soup.find(id="lblStockLatestLastPrice").get_text(strip=True))
+
+            if self.market == 'equity':
+                self._search_equity()
+
+            elif self.market == 'uitf':
+                self._search_uitf()
 
         except Exception:
             return None
+
+    def _search_uitf(self):
+
+        uitf_url = 'http://www.uitf.com.ph/daily_navpu.php?bank_id=9#gsc.tab=0'  # landbank only
+        response = requests.get(uitf_url)
+        soup = bs4.BeautifulSoup(response.content, 'html.parser')
+
+        fund_names = [element.a.get_text(strip=True) for element in
+                      soup.find_all('td', attrs={'data-title': 'Fund Name'})]
+        navpu = [float(element.div.get_text(strip=True)) for element in
+                 soup.find_all('td', attrs={'data-title': 'NAVpu'})]
+
+        landbank = dict(zip(fund_names, navpu))
+
+        self.company = self.ticker
+        self.price = landbank.get(self.ticker, None)
+
+    def _search_equity(self):
+
+        investegram_url = f'https://www.investagrams.com/stock/{self.ticker}'
+        response = requests.get(investegram_url)
+        soup = bs4.BeautifulSoup(response.content, 'html.parser')
+
+        self.company = soup.find('h4', class_='mb-0').find('small').get_text(strip=True)
+        self.price = float(soup.find(id="lblStockLatestLastPrice").get_text(strip=True))
--- tests.py
+++ tests.py
@@ -6,53 +6,57 @@ from mistermarket import MrMarket
 
 class MrMarketTestCase(unittest.TestCase):
 
-    def test_name_if_equal_to_ticker(self):
+    def test_init_arguments(self):
 
-        company = MrMarket('jfc')
-        result = company.ticker
-        expected = 'jfc'
+        company = MrMarket('jfc', market='equity')
+        result_company = company.ticker
+        result_market = company.market
+        expected_company = 'jfc'
+        expected_market = 'equity'
 
-        self.assertEqual(result, expected)
+        self.assertEqual(result_company, expected_company)
+        self.assertEqual(result_market, expected_market)
 
-    def test_search_ticker(self):
+    def test_search_equity(self):
 
         ticker = 'jfc'
-        company = MrMarket(ticker)
+        company = MrMarket(ticker, market='equity')
 
         investegram_url = f'https://www.investagrams.com/stock/{ticker}'
         response = requests.get(investegram_url)
         soup = bs4.BeautifulSoup(response.content, 'html.parser')
+
         expected = float(soup.find(id='lblStockLatestLastPrice').text.strip())
-        print(f'{ticker} current price: {expected}')
         result = company.price
 
         self.assertEqual(result, expected, f'current price: {expected}')
 
     def test_price_return_type_equal_to_float(self):
 
-        stock = MrMarket('ans')
-        result = stock.price
+        stock = MrMarket('ans', market='equity')
+        uitf = MrMarket('LANDBANK Equity Fund', market='uitf')
+        result_stock = stock.price
+        result_uitf = uitf.price
         expected = float
 
-        self.assertIsInstance(result, expected)
+        self.assertIsInstance(result_stock, expected)
+        self.assertIsInstance(result_uitf, expected)
 
-    def test_search_UITFPH_result(self):
+    def test_search_uitf(self):
 
         ticker = 'LANDBANK Money Market Plus Fund'
-        url = {'LANDBANK Money Market Plus Fund': 'http://www.uitf.com.ph/daily_navpu.php?bank_id=9#gsc.tab=0'}
-        response = requests.get(url[ticker])
+        uitf_url = 'http://www.uitf.com.ph/daily_navpu.php?bank_id=9#gsc.tab=0'  # landbank
+        response = requests.get(uitf_url)
         soup = bs4.BeautifulSoup(response.content, 'html.parser')
 
-        fund_names = [element.a.get_text(strip=True) for element in soup.find_all('td', attrs={'data-title': 'Fund Name'})]
-        navpu = [element.div.get_text(strip=True) for element in soup.find_all('td', attrs={'data-title': 'NAVpu'})]
+        fund_names = [element.a.get_text(strip=True) for element in
+                      soup.find_all('td', attrs={'data-title': 'Fund Name'})]
+        navpu = [float(element.div.get_text(strip=True)) for element in
+                 soup.find_all('td', attrs={'data-title': 'NAVpu'})]
 
         landbank = dict(zip(fund_names, navpu))
 
-        for fund, navpu in landbank.items():
-            print(fund, navpu)
-
-        # latest navpu of ticker
-        print(landbank.get(ticker, None))
+        self.price = landbank.get(ticker, None)
 
 
 if __name__ == '__main__':
